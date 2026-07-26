36707ad953c142d2aa2f1e05864fe48e8e33f485
KIRINICE001 <unicornice001@gmail.com>
2026-07-26 20:55:39 +0800

    AnalyticsCard.js

--- themes/hexo/components/AnalyticsCard.js
+++ themes/hexo/components/AnalyticsCard.js
@@ -1,30 +1,58 @@
 import Card from './Card'
 
 export function AnalyticsCard (props) {
-  const { postCount } = props
+
+  const { postCount, wordCount } = props
+
+  // 建站日期
+  const targetDate = new Date('2026-07-01')
+  const today = new Date()
+
+  const diffTime = today.getTime() - targetDate.getTime()
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+
+
   return <Card>
-    <div className='ml-2 mb-3 '>
+    <div className='ml-2 mb-3'>
       <i className='fas fa-chart-area' /> 统计
     </div>
-    <div className='text-xs  font-light justify-center mx-7'>
-      <div className='inline'>
-        <div className='flex justify-between'>
-          <div>文章数:</div>
-          <div>{postCount}</div>
-        </div>
+
+    <div className='text-xs font-light justify-center mx-7'>
+
+      <div className='flex justify-between'>
+        <div>文章数:</div>
+        <div>{postCount}</div>
       </div>
-      <div className='hidden busuanzi_container_page_pv ml-2'>
+
+
+      <div className='flex justify-between'>
+        <div>总字数:</div>
+        <div>{wordCount || 0}</div>
+      </div>
+
+
+      <div className='flex justify-between'>
+        <div>运行时间:</div>
+        <div>{diffDays} 天</div>
+      </div>
+
+
+      <div className='hidden busuanzi_container_page_pv'>
         <div className='flex justify-between'>
           <div>访问量:</div>
           <div className='busuanzi_value_page_pv' />
         </div>
       </div>
-      <div className='hidden busuanzi_container_site_uv ml-2'>
+
+
+      <div className='hidden busuanzi_container_site_uv'>
         <div className='flex justify-between'>
           <div>访客数:</div>
           <div className='busuanzi_value_site_uv' />
         </div>
       </div>
+
     </div>
+
   </Card>
 }
