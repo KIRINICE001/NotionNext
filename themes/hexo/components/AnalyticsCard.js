667fef9dd7623d320d20ba59e8999d0d4be042f9
KIRINICE001 <unicornice001@gmail.com>
2026-07-27 11:15:12 +0800

    AnalyticsCard.js

--- themes/hexo/components/AnalyticsCard.js
+++ themes/hexo/components/AnalyticsCard.js
@@ -14,26 +14,26 @@ export function AnalyticsCard (props) {
 
   return <Card>
     <div className='ml-2 mb-3'>
-      <i className='fas fa-chart-area' /> 统计
+      <i className='fas fa-chart-area' /> 統計
     </div>
 
     <div className='text-base font-light justify-center mx-7'>
 
       <div className='flex justify-between'>
-        <div>文章数:</div>
+        <div>文章數:</div>
         <div>{postCount}</div>
       </div>
 
 
       <div className='flex justify-between'>
-        <div>运行时间:</div>
+        <div>運行時間:</div>
         <div>{diffDays} 天</div>
       </div>
 
 
       <div className='hidden busuanzi_container_page_pv'>
         <div className='flex justify-between'>
-          <div>访问量:</div>
+          <div>訪問量:</div>
           <div className='busuanzi_value_page_pv' />
         </div>
       </div>
@@ -41,7 +41,7 @@ export function AnalyticsCard (props) {
 
       <div className='hidden busuanzi_container_site_uv'>
         <div className='flex justify-between'>
-          <div>访客数:</div>
+          <div>訪客數:</div>
           <div className='busuanzi_value_site_uv' />
         </div>
       </div>
