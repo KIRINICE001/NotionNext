4ce5364cc90f38b2e010023e5cdb5044646fd11b
KIRINICE001 <unicornice001@gmail.com>
2026-07-27 12:28:05 +0800

    PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,11 +6,11 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
-    <div className={`inline text-sm font-serif ${props.className || ''}`}>
+    <div className={`inline text-sm ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
         href='https://github.com/tangly1024/NotionNext'
-        className='underline justify-start'>
+        className='text-gray-400 hover:text-gray-600 transition-colors'>
         NotionNext {siteConfig('VERSION')}
       </a>
       .
